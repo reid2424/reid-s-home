c4820d9ac7766d6cfe95839d324f82350cea21bd
reid2424 <2332161793@qq.com>
2024-11-09 19:09:31 +0800

    Add files via upload

---
+++ py0.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+print("你好，世界")
+'''多行注释'''
+#单行注释
+#ctrl+/ 可以取消或者添加单行注释
+# coding=utf-8
+print ('你好','世界',sep=',')
+
+# coding=utf-8
+print ('你好',end =',')
+print ('世界')
+
+# -*- coding: UTF-8 -*-
+print('你好','世界',sep=',')
